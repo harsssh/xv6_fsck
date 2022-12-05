19da7d77da39448f80705f7b8031e83b46a7039d
harsssh <mizuki.k.aa@m.titech.ac.jp>
2022-12-05 19:54:36 +0900

    validate superblock field

--- src/fs/check/check_superblock.rs
+++ src/fs/check/check_superblock.rs
@@ -1,4 +1,30 @@
 use super::*;
+use crate::fs::error::FSError;
 
 impl SuperBlock {
+    pub fn check_fields(&self) -> Result<(), FSError> {
+        if self.size == FSSIZE as u32 {
+            return Ok(());
+        }
+        if self.nblocks == FSSIZE as u32 {
+            return Ok(());
+        }
+        if self.ninodes == NINODES as u32 {
+            return Ok(());
+        }
+        if self.nlog == LOGSIZE as u32 {
+            return Ok(());
+        }
+        if self.logstart == LOGSTART as u32 {
+            return Ok(());
+        }
+        if self.inodestart == INODESTART as u32 {
+            return Ok(());
+        }
+        if self.bmapstart == BITMAPSTART as u32 {
+            return Ok(());
+        }
+
+        Err(FSError::IncorrectSuperBlockField(self))
+    }
 }
