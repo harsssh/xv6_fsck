2f01aefd08ed255f636fe5a6aab35763d2a58cc8
harsssh <mizuki.k.aa@m.titech.ac.jp>
2022-12-07 04:02:19 +0900

    Update check_inode.rs

--- src/fs/check/check_inode.rs
+++ src/fs/check/check_inode.rs
@@ -13,14 +13,23 @@ impl Dinode {
 }
 
 impl FS {
-    pub fn check_device_numbers(&self, index: usize) -> Result<(), FSError> {
-        let dinode = &self.dinodes[index];
+    fn check_device_numbers_individual(&self, inum: u16) -> Result<(), FSError> {
+        let dinode = &self.dinodes[inum as usize];
         if dinode.typ != FileType::DEV {
             return Ok(());
         }
         if dinode.has_valid_device_numbers() {
             return Ok(());
         }
-        Err(FSError::InvalidDevice(index as u16))
+        Err(FSError::InvalidDevice(inum as u16))
+    }
+
+    pub fn check_device_numbers(&self) -> Result<(), FSError> {
+        for (i, dinode) in self.dinodes.iter().enumerate() {
+            if dinode.typ == FileType::DEV {
+                self.check_device_numbers_individual(i as u16)?;
+            }
+        }
+        Ok(())
     }
 }
