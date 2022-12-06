1e893c2df0bdc379f406a1a5c60318e0e3bd81b0
harsssh <mizuki.k.aa@m.titech.ac.jp>
2022-12-07 03:14:34 +0900

    refactor module structure

--- src/fs/check.rs
+++ src/fs/check.rs
@@ -1,25 +1,8 @@
-use super::*;
-
+mod consts;
 mod check_superblock;
 mod check_block_usage;
 mod check_inode;
 mod check_directory;
 
-/* For consistency check */
-// Size of file system image (blocks)
-const FSSIZE: usize = 2000;
-// Number of inodes
-const NINODES: usize = 200;
-
-// Number of log blocks
-const LOGSIZE: usize = 30;
-// Number of inode blocks
-const NINODEBLOCKS: usize = NINODES / IPB + 1;
-// Number of bitmap blocks
-const NBITMAP: usize = FSSIZE / BPB + 1;
-// Number of data blocks
-const NBLOCKS: usize = FSSIZE - LOGSIZE - NINODEBLOCKS - NBITMAP - 2;
-
-const LOGSTART: usize = 2;
-const INODESTART: usize = LOGSTART + LOGSIZE;
-const BITMAPSTART: usize = INODESTART + NINODEBLOCKS;
+use super::*;
+pub use crate::fs::check::consts::*;
---
+++ src/fs/check/consts.rs
@@ -0,0 +1,20 @@
+use crate::fs::consts::*;
+
+/* For consistency check */
+// Size of file system image (blocks)
+pub const FSSIZE: usize = 2000;
+// Number of inodes
+pub const NINODES: usize = 200;
+
+// Number of log blocks
+pub const LOGSIZE: usize = 30;
+// Number of inode blocks
+pub const NINODEBLOCKS: usize = NINODES / IPB + 1;
+// Number of bitmap blocks
+pub const NBITMAP: usize = FSSIZE / BPB + 1;
+// Number of data blocks
+pub const NBLOCKS: usize = FSSIZE - LOGSIZE - NINODEBLOCKS - NBITMAP - 2;
+
+pub const LOGSTART: usize = 2;
+pub const INODESTART: usize = LOGSTART + LOGSIZE;
+pub const BITMAPSTART: usize = INODESTART + NINODEBLOCKS;
