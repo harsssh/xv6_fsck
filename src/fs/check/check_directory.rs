4daf9486222814976d059a08b1a0570f2738ad4b
harsssh <mizuki.k.aa@m.titech.ac.jp>
2022-12-07 03:56:19 +0900

    Update check_directory.rs

--- src/fs/check/check_directory.rs
+++ src/fs/check/check_directory.rs
@@ -1,2 +1,35 @@
 use super::*;
 use crate::fs::error::FSError;
+
+impl FS {
+    fn check_current_directory_individual(&self, inum: u16) -> Result<(), FSError> {
+        let dirents = match self.get_dirents(&inum) {
+            Some(dirents) => dirents,
+            // not a directory
+            None => return Ok(()),
+        };
+
+        let mut inum_from_dirents = 0;
+        for dirent in dirents {
+            if dirent.name == "." {
+                inum_from_dirents = dirent.inum;
+                break;
+            }
+        }
+
+        if inum_from_dirents == inum {
+            Ok(())
+        } else {
+            Err(FSError::IncorrectCurrentDirRef(inum))
+        }
+    }
+
+    pub fn check_current_directory(&self) -> Result<(), FSError> {
+        for (inum, dinode) in self.dinodes.iter().enumerate() {
+            if dinode.typ == FileType::DIR {
+                self.check_current_directory_individual(inum as u16)?;
+            }
+        }
+        Ok(())
+    }
+}
