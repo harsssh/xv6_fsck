cb1efa598fb35a1c1e5ce85106dba10273d950b3
harsssh <mizuki.k.aa@m.titech.ac.jp>
2022-12-07 03:56:33 +0900

    Update check_directory.rs

--- src/fs/check/check_directory.rs
+++ src/fs/check/check_directory.rs
@@ -32,4 +32,41 @@ impl FS {
         }
         Ok(())
     }
+
+    fn check_parent_directory_individual(&self, inum: u16) -> Result<(), FSError> {
+        let dirents = match self.get_dirents(&inum) {
+            Some(dirents) => dirents,
+            // not a directory
+            None => return Ok(()),
+        };
+
+        let mut inum_from_dirents = 0;
+        for dirent in dirents {
+            if dirent.name == ".." {
+                inum_from_dirents = dirent.inum;
+                break;
+            }
+        }
+
+        let dir = self.get_node(&inum).unwrap();
+        let parents = dir.parents.borrow();
+        assert_eq!(parents.len(), 1);
+        let parent = parents[0].upgrade().unwrap();
+
+        let inum_from_node = parent.value;
+        if inum_from_dirents == inum_from_node {
+            Ok(())
+        } else {
+            Err(FSError::IncorrectParentDirRef(inum))
+        }
+    }
+
+    pub fn check_parent_directory(&self) -> Result<(), FSError> {
+        for (inum, dinode) in self.dinodes.iter().enumerate() {
+            if dinode.typ == FileType::DIR {
+                self.check_parent_directory_individual(inum as u16)?;
+            }
+        }
+        Ok(())
+    }
 }
